c809a5f6b5fd0d5df9e6b72d73b166e30d65e686
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-11-29 16:45:55 +0100

    setup cva and tailwind extend

---
+++ src/components/Button.tsx
@@ -0,0 +1,49 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+const buttonStyles = cva("p-2.5", {
+  variants: {
+    variant: {
+      primary: [
+        "bg-primary",
+        "text-primary-text",
+        "hover:bg-primary-hover",
+        "hover:text-primary-neutral",
+      ],
+      ghost: ["text-ghost-text", "hover:bg-ghost-hover"],
+    },
+    btnType: {
+      button: ["rounded"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+      ],
+    },
+    defaultVariants: {
+      variant: "primary",
+      btnType: "button",
+    },
+  },
+});
+
+type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">;
+
+const Button = ({
+  variant = "primary",
+  btnType = "button",
+  className,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={twMerge(buttonStyles({ variant, btnType }), className)}
+    />
+  );
+};
+export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+import colors from "tailwindcss/colors"
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: colors.orange[300],
+          hover: colors.orange[400],
+          text: colors.orange[500],
+          neutral: colors.neutral[100]
+        },
+        ghost: {
+          hover: colors.neutral[300],
+          text: colors.neutral[500],
+        }
+      },
+    },
   },
   plugins: [],
 }
